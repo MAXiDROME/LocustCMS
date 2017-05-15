2e6b6ac4f9209a2b855243e58e7a60cc2ebd7ad7
maxidrome <mkoreev@mail.ru>
2017-05-15 19:52:12 +0500

    bootstrap 4 added

--- templates/includes/header.inc.php
+++ templates/includes/header.inc.php
@@ -1,12 +1,14 @@
 <!DOCTYPE html>
-<html>
+<html lang="ru">
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title><?=$meta_title?></title>
     <meta name="keywords" content="<?=$meta_ketwords?>">
     <meta name="description" content="<?=$meta_description?>">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/9b9e5f7c41.css">
     <link rel="stylesheet" href="/css/styles.css">
 </head>
 <body>
