b5d45f1000bd5d287a7865aab982922d131a7325
maxidrome <mkoreev@mail.ru>
2017-05-15 13:55:54 +0500

    maintenance page template

--- templates/maintenance.php
+++ templates/maintenance.php
@@ -1,1 +1,13 @@
-Сайт закрыт на обслуживание. Зайдите позже.
+<?
+include_once 'includes/header.inc.php';
+
+include_once 'includes/page.header.inc.php';
+?>
+    <main>
+        <section>
+            <h1>Сайт закрыт на обслуживание</h1>
+            <p>Зайдите позже.</p>
+        </section>
+    </main>
+<?
+include_once 'includes/footer.inc.php';
